d9e5b443caf3078557064f4ff511a6191c2a5c48
Yves Chevallier <yves.chevallier@heig-vd.ch>
2024-03-25 09:37:55 +0100

    My producer-consumer

--- 05 Semaphore/producer-consumer.cpp
+++ 05 Semaphore/producer-consumer.cpp
@@ -5,49 +5,50 @@
 #include <semaphore>
 #include <cstdlib>
 
-#define MAX 100
+#define MAX 20
 static std::vector<int> buffer;
 static std::mutex mtx;
 static std::counting_semaphore<MAX> sem_empty(MAX);
 static std::counting_semaphore<MAX> sem_full(0);
 
-void consumer(int N) {
+void consumer(int consuming_time) {
     while (true) {
-        for (int i = 0; i < N; ++i) {
-            sem_full.acquire();
-        }
+        sem_full.acquire();
         {
             std::lock_guard<std::mutex> lock(mtx);
-            for (int i = 0; i < N; ++i) {
-                if (!buffer.empty()) {
-                    int data = buffer.back();
-                    buffer.pop_back();
-                    std::cout << "Consumer: " << data << std::endl;
-                }
-            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(consuming_time));
+            int data = buffer.back();
+            buffer.pop_back();
+            std::cout << "Buffer [" << buffer.size() << "/" << MAX << "]" << std::endl;                    
+            std::cout << "Consumer: " << data << std::endl;
         }
-        for (int i = 0; i < N; ++i) {
-            sem_empty.release();
-        }
+        sem_empty.release();
     }
 }
 
-void producer() {
+void producer(int manufacturing_time) {
     while (true) {
         const int data = std::rand() % 100;
         sem_empty.acquire();
         {
             std::lock_guard<std::mutex> lock(mtx);
+            std::this_thread::sleep_for(std::chrono::milliseconds(manufacturing_time));
             buffer.push_back(data);
+            std::cout << "Buffer [" << buffer.size() << "/" << MAX << "]" << std::endl;            
             std::cout << "Producer: " << data << std::endl;
         }
         sem_full.release();
     }
 }
 
-int main() {
-    std::thread t1(producer);
-    std::thread t2(consumer, 4); // Suppose that the consumer consumes 4 items at a time
+int main(int argc, char*argv[]) {
+    int manufacturing_time = atoi(argv[1]);
+    int consuming_time = atoi(argv[2]);
+
+    std::thread t1(producer, manufacturing_time);
+    std::thread t2(consumer, consuming_time); 
+    std::thread t3(consumer, consuming_time); 
     t1.join();
+    t2.join();
     t2.join();
 }
